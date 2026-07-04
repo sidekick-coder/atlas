b6042fa1a1c7cf87e57ce885e730fbd56459e13d
Henrique <henrique@sidekick-coder.com>
2026-07-03 22:16:31 -0300

    refactor: remove uneed meta input

--- tui/components/entry_metas.go
+++ tui/components/entry_metas.go
@@ -33,6 +33,19 @@ type EntryMetas struct {
 	height  int
 }
 
+type MetaInputSubmitMsg struct {
+	EntryID int64
+	Name    string
+	Value   string
+}
+
+// MetaOpenEditorMsg is emitted when the user requests to open $EDITOR.
+type MetaOpenEditorMsg struct {
+	EntryID      int64
+	Name         string
+	CurrentValue string
+}
+
 func NewEntryMetas() *EntryMetas {
 	return &EntryMetas{}
 }
