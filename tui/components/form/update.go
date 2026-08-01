50c9d4448b0c7c60363c7eb595985b09c97afadb
Henrique <henrique@sidekick-coder.com>
2026-08-01 20:35:17 -0300

    fix: create-task action

--- tui/components/form/update.go
+++ tui/components/form/update.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sidekick-coder/atlas/tui/action"
 	"github.com/sidekick-coder/atlas/tui/components/toast"
 	"github.com/sidekick-coder/atlas/tui/features/chain"
+	"github.com/sidekick-coder/atlas/tui/features/context"
 )
 
 func (c *Component) Update(msg tea.Msg) tea.Cmd {
@@ -38,13 +39,20 @@ func (c *Component) submit() tea.Cmd {
 	id := c.action.Type
 	opts := map[string]any{}
 
+	ctx, ok := context.GetById("global")
+
+	if !ok {
+		return  toast.Error("Global context is not available")
+	}
+
+	maps.Copy(opts, ctx.GetEntriesMap())
 	maps.Copy(opts, c.action.Options)
 
-	computed, err := template.EvaluateMap(opts, values)
+	opts, err := template.EvaluateMap(opts, values)
 
 	if err != nil {
 		return toast.Error(err.Error())
 	}
 
-	return action.Execute(id, computed)
+	return action.Execute(id, opts)
 }
